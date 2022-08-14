f7b1ec7d064aa263154f2d034f41ddd16f690516
Darrion White <84244339+whid72@users.noreply.github.com>
2022-08-14 15:29:20 -0400

    bug fix

---
+++ Sources/DataFetchFramework.xcframework/ios-arm64_x86_64-simulator/DataFetchFramework.framework/Headers/DataFetchFramework.h
@@ -0,0 +1,18 @@
+//
+//  DataFetchFramework.h
+//  DataFetchFramework
+//
+//  Created by Rave Bizz on 8/10/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DataFetchFramework.
+FOUNDATION_EXPORT double DataFetchFrameworkVersionNumber;
+
+//! Project version string for DataFetchFramework.
+FOUNDATION_EXPORT const unsigned char DataFetchFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DataFetchFramework/PublicHeader.h>
+
+
